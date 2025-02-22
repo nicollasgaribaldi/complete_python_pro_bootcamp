7af962c2baa670b4f80bbdbab35421bd009a861f
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2025-02-21 23:40:26 -0300

    Day 3 1/2

---
+++ Section 3/coding_exercises/exercise.py
@@ -0,0 +1,24 @@
+### BMI Calculator with Interpretations ###
+
+#  Add some if/elif/else statements to the BMI calculator so that it interprets the BMI
+# values calculated.
+
+# If the bmi is under 18.5 (not including), print out "underweight"
+# If the bmi is between 18.5 (including) and 25 (not including), print out "normal weight"
+# If the bmi is 25 (including) or over, print out "overweight"
+
+weight = 85
+height = 1.85
+
+bmi = round(weight / (height ** 2), 2)
+
+# 🚨 Do not modify the values above
+# Write your code below 👇
+
+print(f"That is your BMI:\n{bmi}")
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 < bmi < 25:
+    print("Normal weight")
+else:
+    print("Overweight")
---
+++ Section 3/tasks/task_2.py
@@ -0,0 +1,26 @@
+### MODULO ###
+
+## The modulo operator gives you the remainder of a division.
+
+6 % 2 #will be 0
+
+6 % 5 #will be 1
+
+6 % 4 #will be 2
+
+## 1 - What is 10 % 3?
+#What do you think this will output?
+
+print(10 % 3)
+
+## 2 - Check Odd or Even
+# Write some code using what you have learnt about the modulo operator and conditional
+# checks in Python to check if the number in he input area is odd or even. If it's odd
+# print out the word "Odd" otherwise printout "Even".
+
+modulo = int(input("What number do you want to check?\n"))
+
+if modulo % 2 == 0:
+    print("This number is even.")
+else:
+    print("This number is odd.")
