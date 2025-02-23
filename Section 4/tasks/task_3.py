d57ab94c5cbce8b06ee281d23913afb01066837e
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2025-02-23 17:34:23 -0300

    Day 4 1/2

---
+++ Section 4/tasks/task_3.py
@@ -0,0 +1,5 @@
+### BANKER ROULETTE ###
+
+# Figure out how to pick a random name from the list of friends.
+
+friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
---
+++ Section 4/tasks/task_5.py
@@ -0,0 +1,35 @@
+### ROCK PAPER SCISSORS ###
+
+# You are going to build a Rock, Paper, Scissors game.
+# You will need to use what you have learnt about randomisation and Lists to achieve this.
+
+## Demo
+# https://appbrewery.github.io/python-day4-demo/
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
