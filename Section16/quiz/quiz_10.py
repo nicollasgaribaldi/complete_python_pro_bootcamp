eea18b2dca017e429347dbda9cfcffbe442d3827
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2025-04-16 16:08:01 -0300

    Section 16

---
+++ Section16/quiz/quiz_10.py
@@ -0,0 +1,42 @@
+### PYTHON OBJECTS QUIZ ###
+
+## Question 1:
+# In OOP what is the name of the blueprint for creating objects?
+
+# A) Attribute
+# B) Method
+# C) Class
+# D) Instance
+
+## ANSWER: (C) Class
+
+## Question 2:
+# Given a Class blueprint for a Car has the following attributes and methods, which line of code in the answers will produce an error?
+
+# Attributes:
+# num_of_seats
+# speed
+
+# Methods:
+# drive()
+# brake()
+
+# A) car.drive()
+# B) car.num_of_seats = 2
+# C) car.brake() = 0
+# D) print(car.speed)
+
+## ANSWER: (C) car.brake() = 0
+
+## Question 3:
+# my_toyota = Car()
+# my_fiat = Car()
+# What word would you use to describe what's inside my_toyota and my_fiat?
+
+# A) Class
+# B) Attribute
+# C) Variable
+# D) Object
+# E) Method
+
+## ANSWER: (D) Object
