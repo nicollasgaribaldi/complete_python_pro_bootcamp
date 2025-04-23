27305cc9e7f227f2a1f095dd45c38ceedff96e8f
nicollasgaribaldi <nicollasgaribaldi@hotmail.com>
2025-04-23 15:00:56 -0300

    Day 17

---
+++ Section 17/quiz-game-start/data.py
@@ -0,0 +1,82 @@
+question_data = [
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"The Python programming language gets its name from the British comedy group &quot;Monty Python.&quot;",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"The logo for Snapchat is a Bell.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"Ada Lovelace is often considered the first computer programmer.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"Linus Torvalds created Linux and Git.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"Time on Computers is measured via the EPOX System.",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"The first IBM PC was released in 1981.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"JavaScript derives from a later version of Java",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"The Windows ME operating system was released in the year 2000.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"A Mac is not a PC",
+            "correct_answer":"False",
+            "incorrect_answers":["True"]
+        },
+        {
+            "type":"boolean",
+            "difficulty":"easy",
+            "category":"Science: Computers",
+            "question":"RAM stands for Random Access Memory.",
+            "correct_answer":"True",
+            "incorrect_answers":["False"]
+        }
+    ]
